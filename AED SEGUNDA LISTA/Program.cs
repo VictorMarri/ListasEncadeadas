71c8474711f84b2156cde7a3ec5cb23e8479a292
VictorMarri <victorpujoni@gmail.com>
2021-02-10 23:36:09 -0300

    Primeiro Commit

---
+++ AED SEGUNDA LISTA/Program.cs
@@ -0,0 +1,17 @@
+﻿using AED_SEGUNDA_LISTA.AED;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AED_SEGUNDA_LISTA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPilha a = new CPilha();
+        }
+    }
+}
